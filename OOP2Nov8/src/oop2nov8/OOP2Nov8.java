48ec82ad9a25e46963d672e2b7022442858201a1
ZenaNusair <149420913+ZenaNusair@users.noreply.github.com>
2023-11-11 16:06:55 +0300

    Add files via upload

---
+++ OOP2Nov8/src/oop2nov8/OOP2Nov8.java
@@ -0,0 +1,24 @@
+package oop2nov8;
+
+public class OOP2Nov8 {
+
+    public static void main(String[] args) {
+        Student srd1 = new Student();
+        srd1.setId(10);
+        srd1.setName("zena");
+        srd1.setGpa(4.0);
+    
+    
+Student std2=new Student(20,"zain",4.0);
+
+Student std3=new Student();
+std3.withName("Xena").withId(251).withGpa(3.3);
+std3.withEmail("Z@z");
+        
+        
+        
+    
+
+
+
+}}
